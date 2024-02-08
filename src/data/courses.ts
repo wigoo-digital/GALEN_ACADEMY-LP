60a97e08db7a8656ab1b75561b4545f3c7a47047
Natan <natan.ds18@gmail.com>
2024-02-08 11:08:24 -0300

    mock data for courses pages

---
+++ src/data/courses.ts
@@ -0,0 +1,134 @@
+export const courses = [
+    {
+        "nome": "Pós-graduação em Pesquisa Clínica",
+        "tipo": "Pós-graduação",
+        "disponivel_para_venda": true,
+        "formato_ensino": {
+            "aulas_gravadas": true,
+            "encontros_ao_vivo_mensais": true
+        },
+        "inicio_aulas": "início de março",
+        "publico_alvo": "Profissionais da área da saúde, abrangendo médicos, enfermeiros, fonoaudiólogos, psicólogos, terapeutas ocupacionais, nutricionistas, farmacêuticos, biólogos, biomédicos, gestores e pesquisadores atuantes no campo da saúde.",
+        "objetivo": "A Pós-graduação lato sensu em Pesquisa Clínica é a oportunidade ideal para profissionais de saúde que desejam se especializar e se destacar nesse campo tão promissor. Nosso curso foi cuidadosamente desenvolvido para capacitar você a exercer atividades de alto nível em Pesquisa Clínica, abrangendo diversos campos de atuação. Com um corpo docente renomado e experiente, você terá acesso a conhecimentos fundamentais para a elaboração e condução de protocolos de estudos científicos, além de aprender a interpretar e aplicar metodologias científicas avançadas. Nossos módulos abrangem temas como princípios de estatística, análise de dados, ética em pesquisa e muito mais.",
+        "coordenadores": [
+            "Prof. Dr. Renato Lopes",
+            "Dr. Pedro Barros",
+            "Dr. Remo Furtado",
+            "Lucas Damiani",
+            "Profa. Lilian Mazza"
+        ],
+        "grade_curricular": {
+            "carga_horaria": "360 horas",
+            "duracao": "1 ano",
+            "aula_ao_vivo_mensal": true,
+            "detalhes_pdf": "Grade completa no PDF"
+        },
+        "parcerias_colaboracoes": {
+            "realizadores": ["Galen Academy", "BCRI"],
+            "diferenciais": [
+                "Corpo Docente com sólida experiência clínica e acadêmica",
+                "Parceria internacional com Duke University e Duke Clinical Research Institute (DCRI)",
+                "Grade curricular cuidadosamente planejada incluindo temas clássicos e atuais da área"
+            ]
+        },
+        "valor": {
+            "avista": {
+                "valor": "18.176,40",
+                "desconto_percentual": 10
+            },
+            "parcelado_12x": {
+                "boleto": true,
+                "cartao_credito": true,
+                "valor_parcela": "1.685,00"
+            },
+            "parcelado_18x": {
+                "boleto": true,
+                "cartao_credito": true,
+                "valor_parcela": "1.123,00"
+            }
+        },
+    },
+    {
+        "nome": "Curso de Pós-graduação em Cardio-oncologia",
+        "tipo": "Pós-Graduação",
+        "formato_ensino": {
+            "aulas_ao_vivo": true,
+            "frequencia_aulas_ao_vivo": {
+                "aulas_extras_semanais": {
+                    "online": true,
+                    "duracao": "1 hora por semana"
+                },
+                "aulas_mensais": {
+                    "online": true,
+                    "duracao": "4 horas",
+                    "frequencia": "1 vez por mês"
+                }
+            },
+            "grupo_interacao_alunos": true
+        },
+        "objetivo": 'A Pós-graduação Lato Sensu em Cardio-oncologia é ideal para médicos que desejam se especializar e se destacar nessa área tão promissora e em expansão. Nosso curso proporcionará a capacitação para exercer a cardio-oncologia com segurança e excelência. O conteúdo programático é abrangente e abordará todos temas relacionados a cardio-oncologia. Contamos com um corpo docente renomado e com larga experiência teórica e prática na cardio-oncologia. Durante a formação o aluno terá aulas sobre as diversas doenças oncológicas e adquirirá habilidades para acompanhar e cuidar da saúde cardiovascular das pessoas que tem ou tiveram câncer, sendo capaz de solucionar casos cínicos desafiadores em cardio-oncologia. Além disso nosso programa conta com diversas aulas sobre pesquisa clínica e a oportunidade de aprender com professores com um grande conhecimento e experiência na área de pesquisa.O principal objetivo da Pós-graduação em Cardio-oncologia é habilitar médicos para atuarem na cardio-oncologia com excelência. Para isso buscaremos que nossos alunos:- Saibam que um ponto central na cardio-oncologia é proporcionar que os pacientes com câncer recebam o melhor tratamento oncológico possível com o menor risco de complicações cardiovasculares;- Aprendam conhecimentos gerais sobre todas doenças oncológicas (incluindo as doenças hemato-oncológicas) desde uma perspectiva multidisciplinar;- Aprendam sobre a jornada dos pacientes com câncer e sobre os tratamentos de cada doença oncológica;- Saibam com profundidade as principais toxicidades cardiovasculares associadas aos diversos tratamentos oncológicos;- Aprendam a manejar as complicações agudas e crônicas relacionadas ao tratamento oncológico;- Conheçam a interação entre o câncer e as doenças cardiovasculares;- Possam definir estratégias de avaliação de risco cardiovascular e monitoramento cardiovascular dos pacientes oncológicos;- Conheçam as particularidades relacionadas as diversas doenças cardiovasculares no contexto das pessoas que tem ou tiveram câncer;- Conheçam as particularidades relacionadas ao manejo do câncer nas pessoas que tem doenças cardiovasculares já estabelecidas- Aprendam noções sobre a cardiologia pediátrica e o seguimento dos sobreviventes de câncer;- Valorizem e aprendam sobre a importância das diversas equipes multidisciplinares no acompanhamento dos pacientes oncológicos;- Estejam bem preparados para realizar a prova de certificação internacional em cardio-oncologia da IC-OS.- Tenham além disso uma visão humanizada sobre o exercício da medicina em especial na cardio-oncologia- Tenham, de maneira sólida, as competências fundamentais para interpretar e aplicar de forma correta as evidências científicas disponíveis. Além disso, o aluno será preparado para atuar como investigador em estudos de pesquisa clínica e, de maneira adicional, a pós-graduação irá oferecer oportunidades para o aluno participar de projetos do BCRI (Brazilian Clinical Research Institute) e facilitar o desenvolvimento da carreira de pesquisa clínica.',
+        "metas": [
+            "Saibam proporcionar o melhor tratamento oncológico com o menor risco de complicações cardiovasculares",
+            "Aprendam conhecimentos gerais sobre todas doenças oncológicas desde uma perspectiva multidisciplinar",
+            "Manejem complicações agudas e crônicas relacionadas ao tratamento oncológico",
+            "Tenham uma visão humanizada sobre o exercício da medicina em cardio-oncologia",
+            "Tenham competências fundamentais para interpretar e aplicar corretamente as evidências científicas disponíveis"
+        ],
+        "coordenadores": [
+            "Prof. Dr. Renato Delascio Lopes",
+            "Dr. João Pedro Dutra"
+        ],
+        "grade_curricular": {
+            "carga_horaria": "360 horas",
+            "duracao": "1 ano",
+            "modulos": [
+                {
+                    "nome": "Módulo I",
+                    "carga_horaria": "45 horas",
+                    "temas": [
+                        "Apresentação da cardio-oncologia, contexto atual no Brasil e no mundo",
+                        "Princípios básicos de oncologia (oncogênese, ciclo celular, desenvolvimento tumoral, estadiamento oncológico)",
+                        "Tipos de tratamento oncológicos e sua relação com a cardio-oncologia",
+                        "Definições de cardiotoxicidade, avaliação de risco e estratégias de cardioproteção",
+                        "Multimodalidade de imagem na cardio-oncologia",
+                        "Radioterapia e a cardio-oncologia",
+                        "Cardio-oncologia reversa, farmacogenética, medicina translacional e papel dos biomarcadores na cardio-oncologia",
+                        "Princípios de cirurgia oncológica e avaliação de risco perioperatório na cardio-oncologia",
+                        "Soft skills e seu impacto na carreira e prática médica",
+                        "Cuidados paliativos: o que todo médico deve saber?",
+                        "Como estruturar um serviço de cardio-oncologia",
+                        "Orientações sobre trabalho de conclusão de curso (TCC): escolha do tema e orientação para o projeto científico",
+                        "Fundamentos de pesquisa clínica e medicina baseada em evidências aplicados ao módulo I",
+                        "Tarefas semanais do aluno on demand"
+                    ],
+                    "fundamentos_pesquisa": true,
+                    "elaboracao_tcc": true,
+                    "tarefas_semanais_aluno": "on demand"
+                },
+                {
+                    "nome": "Módulo II",
+                    "carga_horaria": "45 horas",
+                    "temas": [
+                        "Câncer de mama: do básico ao avançado para o não oncologista",
+                        "Tratamentos do câncer de mama (antraciclinas, anti HER, terapia endócrina, inibidores CDK 4/6, radioterapia) e toxicidade cardiovascular",
+                        "Cardiotoxicidade Permissiva",
+                        "Cuidado cardiovascular e o câncer de mama",
+                        "Câncer de próstata: do básico ao avançado para o não oncologista",
+                        "Tratamentos de câncer de próstata e a toxicidade Cardiovascular",
+                        "Cuidado cardiovascular e o câncer de próstata",
+                        "Exercício físico no paciente oncológico",
+                        "Relação médica com a mídia e a divulgação de conhecimento médico na era das redes sociais",
+                        "Fundamentos de pesquisa clínica e medicina baseada em evidências aplicados ao módulo II",
+                        "Elaboração do TCC",
+                        "Tarefas semanais do aluno on demand"
+                    ],
+                    "fundamentos_pesquisa": true,
+                    "elaboracao_tcc": true,
+                    "tarefas_semanais_aluno": "on demand"
+                },
+
+
+            ]
+        },
+    }
+];
